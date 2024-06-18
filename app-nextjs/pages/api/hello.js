423cbe3790001ed30d02471539012d807c9a936c
Dheeraj Bandaru <dbandaru@microsoft.com>
2024-06-18 12:36:24 -0500

    trying vanilla in west us 2

---
+++ app-nextjs/pages/api/hello.js
@@ -0,0 +1,5 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe - 2024-04-17 12:30' })
+}
